47614a6bb1cc70649102f2ae663d23c4d9221d51
IvanIvchenko <iv4koivan@gmail.com>
2022-05-09 12:09:09 +0300

    fixed and refactored add form

--- src/frontend/components/AddSuperhero.js
+++ src/frontend/components/AddSuperhero.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Box, TextField, Button, Typography } from '@material-ui/core';
+import { Grid, Fab } from '@mui/material'
 import ImageSearchIcon from '@mui/icons-material/ImageSearch';
-import Fab from '@mui/material/Fab';
 import getFormDataFromObject from '../helpers/getFormDataFromObject.js'
 import ImagesDropzone from './Dropzone.js'
 import DisplayImagesFilenames from './DisplayImagesFilenames.js'
@@ -57,56 +57,110 @@ function AddSuperhero({ onFormSubmit }) {
     return (
         <Box
             component="form"
-            sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
-            }}
             noValidate
             autoComplete="off"
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
             onSubmit={handleSubmit}
         >
-            <TextField id="nickname" label="Nickname" variant="standard" onChange={handleInputChange} />
-            <TextField id="real_name" label="Real name" variant="standard" onChange={handleInputChange} />
-            <TextField id="origin_description" label="Origin description" variant="standard" onChange={handleInputChange} />
-            <TextField id="superpowers" label="Superpowers" variant="standard" onChange={handleInputChange} />
-            <TextField id="catch_phrase" label="Catch phrase" variant="standard" onChange={handleInputChange} />
-            <label htmlFor="mainImage">
-                <input
-                    style={{ display: 'none' }}
-                    id="mainImage"
-                    name="mainImage"
-                    type="file"
-                    onChange={handleMainImageUpload}
-                />
-
-                <Fab
-                    color="primary"
-                    size="small"
-                    component="span"
-                    aria-label="add"
-                    variant="extended"
-                >
-                    <ImageSearchIcon />  Upload main image
-                </Fab>
-            </label>
-            <ImagesDropzone onDrop={handleImagesUpload} />
-            {formValues.images.length !== 0 ?
-                <>
-                    <Typography color="primary">Images:</Typography>
-                    <DisplayImagesFilenames images={formValues.images} onDelete={handleImageDelete} />
-                </>
-                : null
-            }
-            <Button
-                variant="contained"
-                color="primary"
-                size="medium"
-                type="submit"
+            <Grid
+                container
+                sx={{
+                    '& > :not(style)': { m: 2, width: '1000px' }
+                }}
+                display="flex"
+                flexDirection="column"
+                wrap="nowrap"
             >
-                Submit
-            </Button>
+                <Grid item >
+                    <TextField
+                        id="nickname"
+                        label="Nickname"
+                        variant="standard"
+                        multiline
+                        style={{ maxWidth: '300px', minWidth: '300px' }}
+                        onChange={handleInputChange}
+                    />
+                </Grid>
+                <Grid item >
+                    <TextField
+                        id="real_name"
+                        label="Real name"
+                        variant="standard"
+                        multiline
+                        style={{ maxWidth: '300px', minWidth: '300px' }}
+                        onChange={handleInputChange}
+                    />
+                </Grid>
+                <TextField
+                    id="origin_description"
+                    label="Origin description"
+                    variant="standard"
+                    multiline
+                    minRows={3}
+                    style={{ maxWidth: '100%', minWidth: '100%' }}
+                    onChange={handleInputChange}
+                />
+                <TextField
+                    id="superpowers"
+                    label="Superpowers"
+                    variant="standard"
+                    multiline
+                    minRows={3}
+                    style={{ maxWidth: '100%', minWidth: '100%' }}
+                    onChange={handleInputChange}
+                />
+                <TextField
+                    id="catch_phrase"
+                    label="Catch phrase"
+                    variant="standard"
+                    multiline
+                    minRows={3}
+                    style={{ maxWidth: '100%', minWidth: '100%' }}
+                    onChange={handleInputChange}
+                />
+                <Grid item  style={{textAlign: "center"}}>
+                <label htmlFor="mainImage">
+                    <input
+                        style={{ display: 'none' }}
+                        id="mainImage"
+                        name="mainImage"
+                        type="file"
+                        onChange={handleMainImageUpload}
+                    />
+                    <Fab
+                        style={{ maxWidth: '50%', minWidth: '50%' }}
+                        color="primary"
+                        size="small"
+                        component="span"
+                        aria-label="add"
+                        variant="extended"
+                    >
+                        <ImageSearchIcon />  Upload main image
+                    </Fab>
+                
+                </label>
+                </Grid>
+                <Grid item >
+                    <ImagesDropzone onDrop={handleImagesUpload} />
+                    {formValues.images.length !== 0 ?
+                        <>
+                            <Typography style={{ color: "black" }}>Images:</Typography>
+                            <DisplayImagesFilenames images={formValues.images} onDelete={handleImageDelete} />
+                        </>
+                        : null
+                    }
+                </Grid>
+                <Grid item align="center">
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        size="medium"
+                        type="submit"
+                        style={{ maxWidth: '150px', maxHeight: '30px', minWidth: '150px', minHeight: '30px' }}
+                    >
+                        Submit
+                    </Button>
+                </Grid>
+            </Grid>
         </Box>
     )
 }
